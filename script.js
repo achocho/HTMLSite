aab7ec2f820734e686fa23603163ca75f7970cba
achocho <adrian.peshev2003@gmail.com>
2022-04-21 23:57:52 +0300

    Adding the last changes

---
+++ script.js
@@ -0,0 +1,7 @@
+function Reset()
+{
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("phone").value="";
+    document.getElementById("textAR").value="";
+}
